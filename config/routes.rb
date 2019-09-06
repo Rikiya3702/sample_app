984415271673918f863430ca893ceba118ee58cc
Rikiya <rikiya3702@gmail.com>
2019-09-06 13:34:00 +0000

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root "static_pages#home"
   
   get '/help',   to:'static_pages#help'
@@ -14,4 +18,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :edit, :create, :update]
   end
