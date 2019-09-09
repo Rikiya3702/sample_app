f142b86c141a68d94482e017d00e86f6645a7202
Rikiya <rikiya3702@gmail.com>
2019-09-09 05:14:09 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
     render html: "hello, world!"
   end
   
+  private
+  
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        store_location
+        redirect_to login_url
+      end
+    end
 end
