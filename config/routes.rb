d4cb8c2ef12d1f710a6b8178175418b83c9140c5
Rikiya <rikiya3702@gmail.com>
2019-09-10 03:00:43 +0000

    Add relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root "static_pages#home"
-  
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get '/help',   to:'static_pages#help'
   get '/about',   to:'static_pages#about'
   get '/contact',   to:'static_pages#contact'
@@ -16,8 +13,14 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :edit, :create, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:  true,
-             activated: true)
+              activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -13,11 +14,21 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               activated: true)
+              activated: true,
+               activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
